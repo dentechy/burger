18f3d3dae751b4ddca0eb3f599583f8bf02cccb8
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-07-24 15:35:40 -0400

    adds sql scripts and defines npm dependencies

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon double cheeseburger");
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie burger");
+INSERT INTO burgers (burger_name)
+VALUES ("Turkey burger");
