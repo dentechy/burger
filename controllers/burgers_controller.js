b6de1c4002f4f6804eb4b76b54e54ef192361d95
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-07-25 10:49:06 -0400

    adds burger routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,29 @@
 const express = require("express");
 const burger = require("../models/burger.js");
+const router = express.app();
+
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  burger.selectAll(function(burgerData) {
+    res.render("index", { burger_data: burgerData });
+  });
+});
+
+router.post("/burgers/create", function(req, res) {
+  burger.insertOne(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+router.put("/burgers/update", function(req, res) {
+  burger.updateOne(req.body.burger_id, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,25 @@
 const orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  insertOne: function(name, cb) {
+    orm.insertOne("burgers", [
+        "burger_name", "devoured"
+    ], [
+    name, false
+    ], cb);
+  },
+  updateOne: function(id, cb) {
+    var state = "id=" + id;
+    orm.updateOne("burgers", {
+      devoured: true
+    }, state, cb);
+  }
+};
+
+module.exports = burger;
+
